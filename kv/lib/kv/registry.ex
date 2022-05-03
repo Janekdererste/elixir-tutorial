c6b47f61f28205ef40d5f0c07e3369a7dfac91f9
Janekdererste <janek.laudan@mailbox.org>
2022-05-03 12:44:57 +0200

    more tutorial stuff

--- kv/lib/kv/registry.ex
+++ kv/lib/kv/registry.ex
@@ -3,55 +3,56 @@ defmodule KV.Registry do
 
   ## Client api goes here
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, :ok, opts)
+    server = Keyword.fetch!(opts, :name)
+    GenServer.start_link(__MODULE__, server, opts)
   end
 
   def lookup(server, name) do
-    GenServer.call(server, {:lookup, name})
+    case :ets.lookup(server, name) do
+      [{^name, pid}] -> {:ok, pid}
+      [] -> :error
+    end
   end
 
   def create(server, name) do
-    GenServer.cast(server, {:create, name})
+    GenServer.call(server, {:create, name})
   end
 
   ## GenServer callbacks
 
   @impl true
-  def init(:ok) do
-    names = %{}
+  def init(table) do
+    names = :ets.new(table, [:named_table, read_concurrency: true])
     refs = %{}
     {:ok, {names, refs}}
   end
 
   @impl true
-  def handle_call({:lookup, name}, _from, state) do
-    {names, _} = state
-    {:reply, Map.fetch(names, name), state}
-  end
-
-  @impl true
-  def handle_cast({:create, name}, state) do
-
+  def handle_call({:create, name}, _from, state) do
     {names, refs} = state
+    lookup_result = lookup(names, name)
 
-    if Map.has_key?(names, name) do
-      {:noreply, state}
-    else
-      {:ok, bucket} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
-      ref = Process.monitor(bucket)
-      refs = Map.put(refs, ref, name)
-      names = Map.put(names, name, bucket)
-      {:noreply, {names, refs}}
+    case lookup_result do
+      # if the lookup result is :ok, the name is already in the table
+      {:ok, pid} ->
+        {:reply, pid, {names, refs}}
+
+      # if the result is :error, the name must be put into the table and a new bucket must be created
+      :error ->
+        {:ok, pid} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
+        ref = Process.monitor(pid)
+        refs = Map.put(refs, ref, name)
+        :ets.insert(names, {name, pid})
+        {:reply, pid, {names, refs}}
     end
   end
 
   @impl true
   def handle_info({:DOWN, ref, :process, _pid, _reason}, state) do
-
     references = elem(state, 1)
     names = elem(state, 0)
     {name, refs} = Map.pop(references, ref)
-    names = Map.delete(names, name)
+    names = :ets.delete(names, name)
     {:noreply, {names, refs}}
   end
 
--- kv/test/kv/registry_test.exs
+++ kv/test/kv/registry_test.exs
@@ -1,9 +1,9 @@
 defmodule KV.RegistryTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
 
-  setup do
-    registry = start_supervised!(KV.Registry)
-    %{registry: registry}
+  setup context do
+    _ = start_supervised!({KV.Registry, name: context.test})
+    %{registry: context.test}
   end
 
   test "spawn buckets", %{registry: registry} do
@@ -20,18 +20,22 @@ defmodule KV.RegistryTest do
     KV.Registry.create(registry, "shopping")
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
+
+    # make a synchronus call to ensure, the DOWN message was processed
+    _ = KV.Registry.create(registry, "bogus")
+
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 
   test "removes bucket on crash", %{registry: registry} do
-
     KV.Registry.create(registry, "name")
     {:ok, bucket} = KV.Registry.lookup(registry, "name")
 
     # stop the agent forcefully with a bad exit code
     Agent.stop(bucket, :shutdown)
+
+    # make a synchronus call to ensure, the DOWN message was processed
+    _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "name") == :error
   end
-
-
 end
